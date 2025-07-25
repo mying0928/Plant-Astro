23ff471874b5f6df6448bf2c0228dea856a7f4cd
yi-cheng <mying0928@gmail.com>
2025-07-25 16:41:36 +0800

    Add all migrated project files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://mying0928.github.io',
+  base: '/Plant-Astro', // 我們將部署到新的儲存庫
+  integrations: [tailwind()]
+});
